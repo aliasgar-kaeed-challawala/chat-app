963d85b573e6346f93b6cee52ca7e56848bc5c0e
aliasgar-kaeed-challawala <aliasgarkcdlsl@gmail.com>
2021-06-02 16:40:32 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:chat_app_flutter/screens/welcome_screen.dart';
+import 'package:chat_app_flutter/screens/login_screen.dart';
+import 'package:chat_app_flutter/screens/registration_screen.dart';
+import 'package:chat_app_flutter/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.black54),
+        ),
+      ),
+
+      initialRoute: WelcomeScreen.id,
+      routes: {
+
+        LoginScreen.id : (context) => LoginScreen(),
+        RegistrationScreen.id : (context) => RegistrationScreen(),
+        WelcomeScreen.id : (context) => WelcomeScreen(),
+        ChatScreen.id : (context) => ChatScreen(),
+      },
+    );
+  }
+}
